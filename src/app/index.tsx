78c78479b2f0cc2fd984e10eb35af92989b3b180
joaomalfatti <joao.cursos1210@gmail.com>
2025-03-23 20:14:56 -0300

    feat - Adicionando uma cara no index para aparecer na page inicial

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,2 +1,15 @@
 import { View, Text } from "react-native";
 
+export default function Index() {
+     return (
+          <View style={{
+               flex: 1,
+               justifyContent: "center",
+               alignItems: "center",
+          }}>
+             <Text style={{ fontSize: 22}}>
+               Hello React Native World!
+               </Text>
+          </View>
+     )
+}
