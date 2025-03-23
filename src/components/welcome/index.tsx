723bef180fb8bf331a30394f22059932eab59e9f
joaomalfatti <joao.cursos1210@gmail.com>
2025-03-23 20:52:21 -0300

    Feat - componente de welcome, logo aparecendo quando carrega

---
+++ src/components/welcome/index.tsx
@@ -0,0 +1,9 @@
+import { Image, Text, View } from "react-native";
+
+import { styles } from "./styles"
+
+export function Welcome() {
+     <View>
+          <Image source={require("@/assets/logo.png")} style={styles.logo} />
+     </View>
+}
