44fec65c4feb4fbf8eaa05e76f4068c85fe32daa
joaomalfatti <joao.cursos1210@gmail.com>
2025-03-24 22:27:07 -0300

    feat - Adicionando Steps e Step para tela inicial

---
+++ src/components/step/index.tsx
@@ -0,0 +1,24 @@
+import { colors } from '@/styles/theme';
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+
+import { IconProps } from '@tabler/icons-react-native';
+
+type Props = {
+  title: string;
+  description: string;
+  icon: React.ComponentType<IconProps>;
+};
+
+export function Step({ title, description, icon: Icon }: Props) {
+  return (
+    <View style={styles.container}>
+      {Icon && <Icon size={32} color={colors.red.base} />}
+
+      <View style={styles.details}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.description}>{description}</Text>
+      </View>
+    </View>
+  );
+}
